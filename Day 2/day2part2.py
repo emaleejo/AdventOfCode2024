022a68f5c4aea76fa813a75fadf847f6b867160f
Emalee <c-emalee.holloway@charter.com>
2024-12-03 10:06:00 -0600

    Create day2part2.py

---
+++ Day 2/day2part2.py
@@ -0,0 +1,23 @@
+def is_safe(data):
+    differences = [data[i] - data[i+1] for i in range(len(data) - 1)]
+    if all(0 < x <= 3 for x in differences) or all(-3 <= x < 0 for x in differences):
+        return True
+    return False
+
+def is_safe_with_dampener(data):
+    if is_safe(data):
+        return True
+    for i in range(len(data)):
+        new_data = data[:i] + data[i+1:]
+        if is_safe(new_data):
+            return True
+    return False
+
+input = open("day2.txt")
+input = input.readlines()
+counter = 0
+for report in input:
+    data = list(map(int, report.split()))
+    if is_safe_with_dampener(data):
+        counter += 1
+print(counter)
